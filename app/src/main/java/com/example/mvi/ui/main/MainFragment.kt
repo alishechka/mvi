d02bcfbf9c0d7b12d060b648c359cb677c9b2242
alishechka <alidrake01@gmail.com>
2020-09-03 19:25:32 +0100

    triggering state events

--- app/src/main/java/com/example/mvi/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvi/ui/main/MainFragment.kt
@@ -3,21 +3,56 @@ package com.example.mvi.ui.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvi.R
+import com.example.mvi.model.BlogPost
+import com.example.mvi.ui.main.state.MainStateEvent
+import com.example.mvi.ui.main.state.MainStateEvent.*
 
 class MainFragment : Fragment() {
+    lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main,container,false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
+
+        viewModel = activity?.run {
+            ViewModelProvider(this).get(MainViewModel::class.java)
+        } ?: throw Exception("invalid Activity")
+
+        subscribeObservers()
+    }
+
+    fun subscribeObservers() {
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            println("DEBUG: DataState: ${dataState}")
+            dataState.blogPosts?.let { blogPost ->
+                //set BlogState data
+                viewModel.setBlogListData(blogPost)
+            }
+            dataState.user?.let { user ->
+                //set User data
+                viewModel.setUser(user)
+            }
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+            viewState.blogPosts?.let {
+                println("DEBUG: DataState: ${it}")
+            }
+            viewState.user?.let {
+                println("DEBUG: DataState: ${it}")
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -26,6 +61,19 @@ class MainFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_get_user -> triggerGetUserEvent()
+            R.id.action_get_blogs -> triggerGetBlogsEvent()
+        }
         return super.onOptionsItemSelected(item)
+
+    }
+
+    private fun triggerGetBlogsEvent() {
+        viewModel.setStateEvent(GetBlogPostsEvent())
+    }
+
+    private fun triggerGetUserEvent() {
+        viewModel.setStateEvent(GetUserEvent("1"))
     }
 }
--- app/src/main/java/com/example/mvi/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/mvi/ui/main/MainViewModel.kt
@@ -4,13 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.mvi.model.BlogPost
+import com.example.mvi.model.User
 import com.example.mvi.ui.main.state.MainStateEvent
 import com.example.mvi.ui.main.state.MainStateEvent.*
 import com.example.mvi.ui.main.state.MainViewState
 import com.example.mvi.util.AbsentLiveData
 
 class MainViewModel : ViewModel() {
+    //event that happens
     private val _stateEvent: MutableLiveData<MainStateEvent> = MutableLiveData()
+    //which view you are presenting...data
     private val _viewState: MutableLiveData<MainViewState> = MutableLiveData()
     val viewState: LiveData<MainViewState>
         get() = _viewState
@@ -25,14 +29,76 @@ class MainViewModel : ViewModel() {
     fun handleStateEvent(stateEvent: MainStateEvent): LiveData<MainViewState> {
         return when (stateEvent) {
             is GetBlogPostsEvent -> {
-                AbsentLiveData.create()
+                return object : LiveData<MainViewState>() {
+                    override fun onActive() {
+                        super.onActive()
+                        val blogList: ArrayList<BlogPost> = ArrayList()
+                        blogList.add(
+                            BlogPost(
+                                pk = 0,
+                                title = "Vancouver PNE 2019",
+                                body = "Here is Jess and I at the Vancouver PNE. We ate a lot of food.",
+                                image = "https://cdn.open-api.xyz/open-api-static/static-blog-images/image8.jpg"
+                            )
+                        )
+                        blogList.add(
+                            BlogPost(
+                                pk = 1,
+                                title = "Ready for a Walk",
+                                body = "Here I am at the park with my dogs Kiba and Maizy. Maizy is the smaller one and Kiba is the larger one.",
+                                image = "https://cdn.open-api.xyz/open-api-static/static-blog-images/image2.jpg"
+                            )
+                        )
+                        value = MainViewState(
+                            blogPosts = blogList
+                        )
+                    }
+                }
             }
+
             is GetUserEvent -> {
-                AbsentLiveData.create()
+                return object : LiveData<MainViewState>() {
+                    override fun onActive() {
+                        super.onActive()
+                        val user = User(
+                            email = "mitch@tabian.ca",
+                            username = "mitch",
+                            image = "https://cdn.open-api.xyz/open-api-static/static-random-images/logo_1080_1080.png"
+                        )
+                        value = MainViewState(
+                            user = user
+                        )
+                    }
+                }
             }
+
             is None -> {
-                AbsentLiveData.create()
+                return AbsentLiveData.create()
             }
         }
     }
+
+    fun setBlogListData(blogPost: List<BlogPost>) {
+        val update = getCurrentViewStateOrNew()
+        update.blogPosts = blogPost
+        _viewState.value = update
+    }
+
+    fun setUser(user: User) {
+        val update = getCurrentViewStateOrNew()
+        update.user = user
+        _viewState.value = update
+    }
+
+    fun getCurrentViewStateOrNew(): MainViewState {
+        val value = viewState.value?.let {
+            it
+        } ?: MainViewState()
+        return value
+    }
+
+    fun setStateEvent(event: MainStateEvent) {
+        _stateEvent.value = event
+    }
+
 }
